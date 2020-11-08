f4f0231f6ded3c083206d5ae3e0c64bb1ed81904
n01326482 <n01326482@humbermail.ca>
2020-11-07 20:27:53 -0500

    first commit

---
+++ ayobamilab2/src/main/java/ayobami/oyesanya/n01326482/AyobamiActivity.java
@@ -0,0 +1,18 @@
+package ayobami.oyesanya.n01326482;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+/**
+ * @author Ayobami Oyesanya
+ * @student_id n01326482
+ */
+public class AyobamiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
