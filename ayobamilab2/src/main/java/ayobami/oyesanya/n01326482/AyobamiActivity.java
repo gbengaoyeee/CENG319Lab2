afad1b29a9e3344575daa79e78f5447c0a271680
n01326482 <n01326482@humbermail.ca>
2020-11-09 14:45:51 -0500

    added second screen

--- ayobamilab2/src/main/java/ayobami/oyesanya/n01326482/AyobamiActivity.java
+++ ayobamilab2/src/main/java/ayobami/oyesanya/n01326482/AyobamiActivity.java
@@ -2,7 +2,10 @@ package ayobami.oyesanya.n01326482;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * @author Ayobami Oyesanya
@@ -13,6 +16,14 @@ public class AyobamiActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_ayobami);
+
+        Button enter = findViewById(R.id.mainScreenEnter);
+        enter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent secondScreen =
+            }
+        });
     }
 }
---
+++ ayobamilab2/src/main/java/ayobami/oyesanya/n01326482/OyesanyaSecondScreen.java
@@ -0,0 +1,14 @@
+package ayobami.oyesanya.n01326482;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class OyesanyaSecondScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
