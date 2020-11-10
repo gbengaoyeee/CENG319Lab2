72f3040821711f5b1883e25df14dd17f672966bc
n01326482 <n01326482@humbermail.ca>
2020-11-10 01:46:17 -0500

    got to make check box working

---
+++ ayobamilab2/src/main/java/ayobami/oyesanya/n01326482/Home.java
@@ -0,0 +1,66 @@
+package ayobami.oyesanya.n01326482;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+/**
+ * @author Ayobami Oyesanya
+ * @student_id n01326482
+ */
+public class Home {
+    String address, price;
+    int image;
+    public Home(String address, String price, int image){
+        this.address = address;
+        this.price = price;
+        this.image = image;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Home home = (Home) o;
+        return image == home.image &&
+                Objects.equals(address, home.address) &&
+                Objects.equals(price, home.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(address, price, image);
+    }
+}
+
+class ApartmentList {
+    Home[] apartmentList = new Home[4];
+
+    public ApartmentList(){
+        apartmentList[0] = new Home("170 Sudbury St, Toronto, Ontario", "$524,000", R.drawable.apartment1);
+        apartmentList[1] = new Home("16 Crocker Ave, Toronto, Ontario", "$1,524,000", R.drawable.apartment2);
+        apartmentList[2] = new Home("20 Jays Way, Toronto, Ontario", "$724,000", R.drawable.apartment3);
+        apartmentList[3] = new Home("97 Front St W, Toronto, Ontario", "$724,000", R.drawable.apartment4);
+    }
+}
+
+class DetachedList {
+    Home[] detachedList = new Home[4];
+
+    public DetachedList(){
+        detachedList[0] = new Home("55 Bowie Ave, Toronto, Ontario", "$524,000", R.drawable.detached1);
+        detachedList[1] = new Home("36 Cadorna Ave, Toronto, Ontario", "$774,000", R.drawable.detached2);
+        detachedList[2] = new Home("392 Quebec Avenue, Toronto, Ontario", "$624,000", R.drawable.detached3);
+        detachedList[3] = new Home("15 Stafford St, Toronto, Ontario\n", "$1,804,000", R.drawable.detached4);
+    }
+}
+
+class SemiDetachedList {
+    Home[] semidetachedList = new Home[4];
+
+    public SemiDetachedList(){
+        semidetachedList[0] = new Home("55 Keele Ave, Toronto, Ontario", "$5,444,000", R.drawable.semidetached1);
+        semidetachedList[1] = new Home("36 Rue Ave, Toronto, Ontario", "$524,000", R.drawable.semidetached2);
+        semidetachedList[2] = new Home("392 Montreal Avenue, Toronto, Ontario", "$924,000", R.drawable.semidetached3);
+        semidetachedList[3] = new Home("15 Rutherford St, Toronto, Ontario\n", "$704,000", R.drawable.semidetached4);
+    }
+}
--- ayobamilab2/src/main/java/ayobami/oyesanya/n01326482/OyesanyaSecondScreen.java
+++ ayobamilab2/src/main/java/ayobami/oyesanya/n01326482/OyesanyaSecondScreen.java
@@ -7,9 +7,14 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -25,15 +30,159 @@ public class OyesanyaSecondScreen extends AppCompatActivity {
 
     ListView listView;
     MenuItem currentSelectedItem;
+
+    CheckBox checkBox1, checkBox2, checkBox3, checkBox4;
+    ImageView homeImage1, homeImage2, homeImage3, homeImage4;
+    TextView address1, address2, address3, address4;
+    TextView price1, price2, price3, price4;
+
+    ApartmentList apartmentList = new ApartmentList();
+    DetachedList detachedList = new DetachedList();
+    SemiDetachedList semiDetachedList = new SemiDetachedList();
+
+    HashSet<Home> selectedHomes = new HashSet<>();
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Set initial screen to be visible
         setContentView(R.layout.activity_oyesanya_second_screen);
         ConstraintLayout initialScreen = findViewById(R.id.initialSecondScreen);
         initialScreen.setVisibility(View.VISIBLE);
         ConstraintLayout updatedScreen = findViewById(R.id.updatedSecondScreen);
         updatedScreen.setVisibility(View.GONE);
 
+        // Setting components
+        checkBox1 = findViewById(R.id.checkbox1);
+        checkBox2 = findViewById(R.id.checkbox2);
+        checkBox3 = findViewById(R.id.checkbox3);
+        checkBox4 = findViewById(R.id.checkbox4);
+
+        homeImage1 = findViewById(R.id.homeImage1);
+        homeImage2 = findViewById(R.id.homeImage2);
+        homeImage3 = findViewById(R.id.homeImage3);
+        homeImage4 = findViewById(R.id.homeImage4);
+
+        address1 = findViewById(R.id.address1);
+        address2 = findViewById(R.id.address2);
+        address3 = findViewById(R.id.address3);
+        address4 = findViewById(R.id.address4);
+
+        price1 = findViewById(R.id.price1);
+        price2 = findViewById(R.id.price2);
+        price3 = findViewById(R.id.price3);
+        price4 = findViewById(R.id.price4);
+
+        //Handle Checkbox checks
+        checkBox1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkBox1.isChecked()){
+                    switch (currentSelectedItem.getItemId()){
+                        case R.id.apartmentMenuOption:
+                            selectedHomes.add(apartmentList.apartmentList[0]);
+                            break;
+                        case R.id.detachedMenuOption:
+                            selectedHomes.add(detachedList.detachedList[0]);
+                            break;
+                        case R.id.semiDetachedMenuOption:
+                            selectedHomes.add(semiDetachedList.semidetachedList[0]);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else{
+                    switch (currentSelectedItem.getItemId()){
+                        case R.id.apartmentMenuOption:
+                            selectedHomes.remove(apartmentList.apartmentList[0]);
+                            break;
+                        case R.id.detachedMenuOption:
+                            selectedHomes.remove(detachedList.detachedList[0]);
+                            break;
+                        case R.id.semiDetachedMenuOption:
+                            selectedHomes.remove(semiDetachedList.semidetachedList[0]);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                Toast.makeText(OyesanyaSecondScreen.this, ""+selectedHomes.size(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        checkBox2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkBox2.isChecked()){
+                    switch (currentSelectedItem.getItemId()){
+                        case R.id.apartmentMenuOption:
+                            selectedHomes.add(apartmentList.apartmentList[1]);
+                            break;
+                        case R.id.detachedMenuOption:
+                            selectedHomes.add(detachedList.detachedList[1]);
+                            break;
+                        case R.id.semiDetachedMenuOption:
+                            selectedHomes.add(semiDetachedList.semidetachedList[1]);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else{
+                    switch (currentSelectedItem.getItemId()){
+                        case R.id.apartmentMenuOption:
+                            selectedHomes.remove(apartmentList.apartmentList[1]);
+                            break;
+                        case R.id.detachedMenuOption:
+                            selectedHomes.remove(detachedList.detachedList[1]);
+                            break;
+                        case R.id.semiDetachedMenuOption:
+                            selectedHomes.remove(semiDetachedList.semidetachedList[1]);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                Toast.makeText(OyesanyaSecondScreen.this, ""+selectedHomes.size(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        checkBox3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(checkBox1.isChecked()){
+                    switch (currentSelectedItem.getItemId()){
+                        case R.id.apartmentMenuOption:
+                            selectedHomes.add(apartmentList.apartmentList[2]);
+                            break;
+                        case R.id.detachedMenuOption:
+                            selectedHomes.add(detachedList.detachedList[2]);
+                            break;
+                        case R.id.semiDetachedMenuOption:
+                            selectedHomes.add(semiDetachedList.semidetachedList[2]);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else{
+                    switch (currentSelectedItem.getItemId()){
+                        case R.id.apartmentMenuOption:
+                            selectedHomes.remove(apartmentList.apartmentList[2]);
+                            break;
+                        case R.id.detachedMenuOption:
+                            selectedHomes.remove(detachedList.detachedList[2]);
+                            break;
+                        case R.id.semiDetachedMenuOption:
+                            selectedHomes.remove(semiDetachedList.semidetachedList[2]);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                Toast.makeText(OyesanyaSecondScreen.this, ""+selectedHomes.size(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -53,18 +202,59 @@ public class OyesanyaSecondScreen extends AppCompatActivity {
         switch (item.getItemId()){
             case R.id.apartmentMenuOption:
                 //do apartment here
+                homeImage1.setImageResource(apartmentList.apartmentList[0].image);
+                homeImage2.setImageResource(apartmentList.apartmentList[1].image);
+                homeImage3.setImageResource(apartmentList.apartmentList[2].image);
+                homeImage4.setImageResource(apartmentList.apartmentList[3].image);
+
+                address1.setText(apartmentList.apartmentList[0].address);
+                address2.setText(apartmentList.apartmentList[1].address);
+                address3.setText(apartmentList.apartmentList[2].address);
+                address4.setText(apartmentList.apartmentList[3].address);
+
+                price1.setText(apartmentList.apartmentList[0].price);
+                price2.setText(apartmentList.apartmentList[1].price);
+                price3.setText(apartmentList.apartmentList[2].price);
+                price4.setText(apartmentList.apartmentList[3].price);
                 return true;
             case R.id.detachedMenuOption:
                 //do detached here
+                homeImage1.setImageResource(detachedList.detachedList[0].image);
+                homeImage2.setImageResource(detachedList.detachedList[1].image);
+                homeImage3.setImageResource(detachedList.detachedList[2].image);
+                homeImage4.setImageResource(detachedList.detachedList[3].image);
+
+                address1.setText(detachedList.detachedList[0].address);
+                address2.setText(detachedList.detachedList[1].address);
+                address3.setText(detachedList.detachedList[2].address);
+                address4.setText(detachedList.detachedList[3].address);
+
+                price1.setText(detachedList.detachedList[0].price);
+                price2.setText(detachedList.detachedList[1].price);
+                price3.setText(detachedList.detachedList[2].price);
+                price4.setText(detachedList.detachedList[3].price);
                 return true;
             case R.id.semiDetachedMenuOption:
                 //do semidetached here
+                homeImage1.setImageResource(semiDetachedList.semidetachedList[0].image);
+                homeImage2.setImageResource(semiDetachedList.semidetachedList[1].image);
+                homeImage3.setImageResource(semiDetachedList.semidetachedList[2].image);
+                homeImage4.setImageResource(semiDetachedList.semidetachedList[3].image);
+
+                address1.setText(semiDetachedList.semidetachedList[0].address);
+                address2.setText(semiDetachedList.semidetachedList[1].address);
+                address3.setText(semiDetachedList.semidetachedList[2].address);
+                address4.setText(semiDetachedList.semidetachedList[3].address);
+
+                price1.setText(semiDetachedList.semidetachedList[0].price);
+                price2.setText(semiDetachedList.semidetachedList[1].price);
+                price3.setText(semiDetachedList.semidetachedList[2].price);
+                price4.setText(semiDetachedList.semidetachedList[3].price);
                 return true;
             default:
                 break;
         }
         return super.onOptionsItemSelected(item);
     }
-
 }
 
